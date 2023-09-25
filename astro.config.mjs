4768096e6bd6f4cfdfae4411e9094fd44ae4750b
timdimas <dimasefthymios@gmail.com>
2023-09-25 12:38:50 +0300

    Remove Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: "https://capitalcompass.gr",
-    base: "/Capital-Compass", // for GitHub Pages
     integrations: [tailwind()],
 });
